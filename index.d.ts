31a5fa3d0a196d3687cd99f89332d0a81790d4c4
Thomas <thomas@shiftybadger.co.uk>
2022-10-14 15:40:28 +0100

    Added type script function definition

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,8 @@ declare function sanitize(
   input: string,
   options?: {
     replacement?: string | ((substring: string) => string);
+    truncateNumber?: integer;
+    convertWhiteSpace?: string | ((substring: string) => string);
   }
 ): string;
 
